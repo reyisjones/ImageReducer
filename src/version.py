0f9a83ba84df7600626796614740b82ad267e09b
reyisjones <reyisj@gmail.com>
2025-10-22 20:53:33 -0400

    chore(release): v1.0.2\n\nIncludes fix to prevent duplicate outputs when compressing folders/files.

--- src/version.py
+++ src/version.py
@@ -2,7 +2,7 @@
 Version information for Image Compressor application.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __version_info__ = tuple(map(int, __version__.split(".")))
 
 # Application metadata
